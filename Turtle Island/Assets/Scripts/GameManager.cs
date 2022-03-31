ef06ae40a1f1b4371ad6fc7af440842e6d47b5fb
Josh Verhoeff <jverhoeff98@gmail.com>
2022-03-31 03:35:52 -0400

    Fixed bug with NPC interaction

--- Turtle Island/Assets/Scripts/GameManager.cs
+++ Turtle Island/Assets/Scripts/GameManager.cs
@@ -5,10 +5,22 @@ public class GameManager : MonoBehaviour {
 
     // Private Game objects
     // UI elements
+    [Header("UI Elements")]
     [SerializeField] private GameObject chatBox;
     [SerializeField] private TextMeshProUGUI chat;
+    [SerializeField] private GameObject buttons;
+
+    public GameObject currentNPC {
+        get {
+            return currentNPC;
+        }
+        set {
+            currentNPC = value;
+        }
+    }    
 
     private void Start() {
+        buttons.SetActive(false);
         chatBox.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -21,11 +33,32 @@ public class GameManager : MonoBehaviour {
     public void hideChatBox() {
         chat.text = "";
         chatBox.SetActive(false);
+        currentNPC = null;
     }
 
     public void updateChatbox(string textToDisplay) {
         if (chatBox.activeInHierarchy) {
             chat.text = textToDisplay;
         }
+    }
+
+    public void hideButtons() {
+        buttons.SetActive(false);
+    }
+
+    public void showButtons() {
+        buttons.SetActive(true);
+    }
+
+    public void Yes() {
+        currentNPC.GetComponent<NPC>().StartActivity();
+    }
+
+    public void No() {
+        currentNPC.GetComponent<NPC>().CancelActivity();
+    }
+
+    public void HowTo() {
+        currentNPC.GetComponent<NPC>().DisplayActivityInstructions();
     }
 }
--- Turtle Island/Assets/Scripts/NPC.cs
+++ Turtle Island/Assets/Scripts/NPC.cs
@@ -1,50 +1,88 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NPC : MonoBehaviour {
+    [Header("General Settings")]
+    [SerializeField] private GameManager manager;
+    [SerializeField] private Interact playerInteract; 
+
     [Header("NPC Settings")]
     [SerializeField] private GameObject player;
     [SerializeField] private List<string> dialogue;
+    [SerializeField] private bool playerSelection;
 
-    private Interact playerInteract;   
-    private GameManager manager; 
+    [Header("Chat Settings")]
+    [SerializeField] private float chatDelay = 2f;
     
     private int currentIndex = 0;
     private bool isChatting = false;
-
-    private void Awake() {
-        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
-    }
-
-    private void Start() {
-        playerInteract = player.GetComponent<Interact>();
-    }
+    private bool chatPause = false;   
 
     private void Update() {
-        if (isChatting && playerInteract.interact) {
-            if (currentIndex < dialogue.Count) {
+        if (isChatting && playerInteract.interact && !chatPause) {
+            if (currentIndex < dialogue.Count - 1) {
                 currentIndex++;
                 manager.updateChatbox(dialogue[currentIndex]);
+                //Debug.Log("Update");
+                StartCoroutine("ChatDelay", chatDelay);
             } else {
-                manager.hideChatBox();
-                isChatting = false;
-                currentIndex = 0;
+                StartCoroutine("PlayerChoice", chatDelay);
             }
         }
     }
 
     private void OnTriggerStay(Collider other) {
-        if(other.CompareTag("Player") && playerInteract.interact && !isChatting) {
+        if(other.CompareTag("Player") && playerInteract.interact && !isChatting && !chatPause && manager.currentNPC == null) {
+            manager.hideButtons();
             manager.displayChatbox(dialogue[currentIndex]);
+            manager.currentNPC = gameObject;
             isChatting = true;
+            //Debug.Log("Trigger");
+            StartCoroutine("ChatDelay", chatDelay);
         }
     }
 
     private void OnTriggerExit(Collider other) {
-        if(other.CompareTag("Player")) {
-            currentIndex = 0;
+        if (other.CompareTag("Player")) {
+            currentIndex = 0;            
             manager.hideChatBox();
             isChatting = false;
         }
+    }
+
+    IEnumerator ChatDelay(float delay) {
+        chatPause = true;
+        //Debug.Log("Delay");
+        yield return new WaitForSeconds(delay);
+        chatPause = false;
+    }
+
+    IEnumerator PlayerChoice(float delay) {
+        currentIndex = 0;
+        chatPause = true;
+        if(!playerSelection) {
+            manager.hideChatBox();
+            isChatting = false;
+        } else {
+            Cursor.lockState = CursorLockMode.None;
+            player.GetComponent<StarterAssets.ThirdPersonController>().LockCameraPosition = true;
+            manager.showButtons();
+        }
+
+        yield return new WaitForSeconds(delay);
+        chatPause = false;
+    }
+
+    public void StartActivity() {
+        Debug.Log("Start Activity");
+    }
+
+    public void DisplayActivityInstructions() {
+        Debug.Log("Display How to");
+    }
+
+    public void CancelActivity() {
+        Debug.Log("Canceled Activity");
     }
 }
