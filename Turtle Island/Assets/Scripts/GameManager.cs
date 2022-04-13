ae2db507ee19d232243ddddc0d9fa4533aceac6f
Josh Verhoeff <jverhoeff98@gmail.com>
2022-04-13 19:31:03 -0400

    Added Dialogue tree system to NPC's

--- Turtle Island/Assets/Scripts/GameManager.cs
+++ Turtle Island/Assets/Scripts/GameManager.cs
@@ -7,44 +7,14 @@ public class GameManager : MonoBehaviour {
 
     // UI elements
     [Header("UI Elements")]
-    [SerializeField] private GameObject chatBox;
-    [SerializeField] private TextMeshProUGUI chat;
-    [SerializeField] private GameObject buttons;
     [SerializeField] private TextMeshProUGUI prompt;
 
     public GameObject currentNPC = null;
 
 
     private void Start() {
-        buttons.SetActive(false);
-        chatBox.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
         hidePrompt();
-    }
-
-    public void displayChatbox(string textToDisplay) {
-        chatBox.SetActive(true);
-        chat.text = textToDisplay;
-    }
-
-    public void hideChatBox() {
-        chat.text = "";
-        chatBox.SetActive(false);
-        currentNPC = null;
-    }
-
-    public void updateChatbox(string textToDisplay) {
-        if (chatBox.activeInHierarchy) {
-            chat.text = textToDisplay;
-        }
-    }
-
-    public void hideButtons() {
-        buttons.SetActive(false);
-    }
-
-    public void showButtons() {
-        buttons.SetActive(true);
     }
 
     public void setPrompt(string textToDisplay) {
@@ -53,17 +23,5 @@ public class GameManager : MonoBehaviour {
 
     public void hidePrompt() {
         prompt.text = "";
-    }
-
-    public void Yes() {
-        currentNPC.GetComponent<NPC>().StartActivity();
-    }
-
-    public void No() {
-        currentNPC.GetComponent<NPC>().CancelActivity();
-    }
-
-    public void HowTo() {
-        currentNPC.GetComponent<NPC>().DisplayActivityInstructions();
     }
 }
--- Turtle Island/Assets/Scripts/NPC.cs
+++ Turtle Island/Assets/Scripts/NPC.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DialogueEditor;
 
 public class NPC : MonoBehaviour {
     [Header("General Settings")]
@@ -11,75 +12,30 @@ public class NPC : MonoBehaviour {
 
     [Header("NPC Settings")]
     [SerializeField] private GameObject player;
-    [SerializeField] private List<string> dialogue;
-    [SerializeField] private bool playerSelection;
 
     [Header("Chat Settings")]
     [SerializeField] private float chatDelay = 2f;
-    
-    private int currentIndex = 0;
-    private bool isChatting = false;
-    private bool chatPause = false;   
-
-    private void Update() {
-        if (isChatting && playerInteract.interact && !chatPause) {
-            if (currentIndex < dialogue.Count - 1) {
-                currentIndex++;
-                manager.updateChatbox(dialogue[currentIndex]);
-                //Debug.Log("Update");
-                StartCoroutine("ChatDelay", chatDelay);
-            } else {
-                StartCoroutine("PlayerChoice", chatDelay);
-            }
-        }
-    }
+    [SerializeField] NPCConversation conversation;
 
     private void OnTriggerStay(Collider other) {
         if (other.CompareTag("Player")) {
             manager.setPrompt("Press Q to talk");
-            if (playerInteract.interact && !isChatting && !chatPause) {
-                manager.hideButtons();
-                manager.displayChatbox(dialogue[currentIndex]);
-                manager.currentNPC = gameObject;
-                isChatting = true;
-                //Debug.Log("Trigger");
-                StartCoroutine("ChatDelay", chatDelay);
+            if (playerInteract.interact) {
+                // Debug.Log("Trigger");
+                Cursor.lockState = CursorLockMode.None;
+                player.GetComponent<StarterAssets.ThirdPersonController>().LockCameraPosition = true;
+                ConversationManager.Instance.StartConversation(conversation);
             }
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.CompareTag("Player")) {
-            currentIndex = 0;            
-            manager.hideChatBox();
-            isChatting = false;
-            manager.hidePrompt();
+            ConversationManager.Instance.EndConversation();
+            Cursor.lockState = CursorLockMode.Locked;
         }
     }
-
-    IEnumerator ChatDelay(float delay) {
-        chatPause = true;
-        //Debug.Log("Delay");
-        yield return new WaitForSeconds(delay);
-        chatPause = false;
-    }
-
-    IEnumerator PlayerChoice(float delay) {
-        currentIndex = 0;
-        chatPause = true;
-        if(!playerSelection) {
-            manager.hideChatBox();
-            isChatting = false;
-        } else {
-            Cursor.lockState = CursorLockMode.None;
-            player.GetComponent<StarterAssets.ThirdPersonController>().LockCameraPosition = true;
-            manager.showButtons();
-        }
-
-        yield return new WaitForSeconds(delay);
-        chatPause = false;
-    }
-
+    
     public void StartActivity() {
         StartCoroutine("LoadSceneAsync", activitySceneName);
     }
@@ -89,10 +45,8 @@ public class NPC : MonoBehaviour {
     }
 
     public void CancelActivity() {
-        //Debug.Log("Canceled Activity");
+        Debug.Log("Canceled Activity");
         Cursor.lockState = CursorLockMode.Locked;
-        manager.hideChatBox();
-        isChatting = false;
         player.GetComponent<StarterAssets.ThirdPersonController>().LockCameraPosition = false;
         StartCoroutine("ChatDelay", chatDelay);
     }
